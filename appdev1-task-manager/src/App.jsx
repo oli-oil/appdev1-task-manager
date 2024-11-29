3eb41ce40114f5baa0f1c92aaf2202c0901d794f
oli.oil <princessolingay@gmail.com>
2024-11-29 20:40:59 +0800

    part 1/4 kasi hindi ko na kaya

---
+++ appdev1-task-manager/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState([
+    {
+      title: "",
+      description: "",
+      status: "pending" or "completed"
+    },
+  ])
+
+  return (
+    <>
+      <div>
+        
+      </div>
+    
+    </>
+  )
+}
+
+export default App
